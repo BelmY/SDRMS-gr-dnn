d9ab29a57bddefae928eedc3004de254411b30b4
Kevin JOLY <kevin.joly@heig-vd.ch>
2019-11-20 11:23:58 +0100

    Add gnuradio-uhd image

--- docker/build-gnuradio.sh
+++ docker/build-gnuradio.sh
@@ -5,7 +5,9 @@ set -e
 docker build -t gnuradio:base gnuradio-base
 # Create pluto image to include pluto support to gnuradio 3.8
 docker build -t gnuradio:pluto gnuradio-pluto
-# Create dev image to include onnx support 
+# Create uhd image to include UHD support to gnuradio 3.8
+docker build -t gnuradio:uhd gnuradio-uhd
+# Create dev image to include onnx support
 docker build -t gnuradio:onnx  gnuradio-onnx
 # Create cuda image to include support for CUDA 10.1
 docker build -t gnuradio:cuda gnuradio-cuda
